bcd751f667e90358b3283964b5fefee3d991e44a
mrutyunjaya <mrutyunjayas490@gmail.com>
2024-11-24 17:08:33 +0530

    Alert added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import About from './components/About.js';
+import Alert from './components/Alert.js';
 import Navbar from './components/Navbar.js'
 import TextForm from './components/TextForm.js'
 import React, {useState} from 'react';
@@ -7,21 +8,36 @@ import React, {useState} from 'react';
 function App() {
   const [Mode, setMode] = useState('light');
 
+  const[alert, setAlert] = useState(null);
+  const showAlert =(message, type)=>{
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(()=>{
+      setAlert(null);
+    },1500);
+  }
+
   const toggleMode =()=>{
     if(Mode ==='light'){
       setMode('dark');
+      document.body.style.backgroundColor = 'black';
+      showAlert("Dark mode has been enabled","success");
     }
     else{
       setMode('light');
+      document.body.style.backgroundColor = 'light';
+      showAlert("Light mode has been enabled","success");
     }
   }
   return (
     <>
     <Navbar title='TextUtils' aboutText='About TextUtils' mode={Mode} toggleMode={toggleMode} />
-
+    <Alert alert={alert}/>
     <div className="container my-3">
-    <TextForm heading='Enter your text to analyze'/>
-    <About />
+    <TextForm showAlert={showAlert} heading='Enter your text to analyze' mode={Mode}/>
+    <About mode={Mode}/>
     </div>
     </> //it's a stucture called js stucture <>every thing under this is a js file</>
   );// here we return jxs file which is look like a html but it as a javascript 
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -6,18 +6,21 @@ export default function TextForm(props) {
         //console.log("Uppercase was clicked: " + text);
         let newText = text.toUpperCase();
         setText(newText);
+        props.showAlert("Coverted to uppercase","success")
     }
 
     const handleLoClick = ()=>{
         //console.log("Uppercase was clicked: " + text);
         let newText = text.toLowerCase();
         setText(newText);
+        props.showAlert("Coverted to lowercase","success")
     }
 
     const handleClearClick = ()=>{
         //console.log("Uppercase was clicked: " + text);
         let newText = '';
         setText(newText);
+        props.showAlert("Clear","success")
     }
 
     const handleOnChange = (event)=>{
@@ -30,21 +33,21 @@ export default function TextForm(props) {
     //setText("new text");// correct way to change the text state
     return (
         <>
-        <div className='container'>
+        <div className='container' style={{color: props.mode==='dark'?'white':'black'}}>
             <h1>{props.heading}</h1>
             <div className="mb-3">
-            <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8" placeholder='Enter your text'></textarea>
+            <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='light'?'gray':'white' , color: props.mode==='dark'?'white':'black'}} id="myBox" rows="8" placeholder='Enter your text'></textarea>
             </div>
             <button className ="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
             <button className ="btn btn-primary mx-2" onClick={handleLoClick}>Convert to Lowercase</button>
             <button className ="btn btn-primary mx-2" onClick={handleClearClick}>Clear Text</button>
     </div>
-    <div className="container my-2">
+    <div className="container my-2" style={{color: props.mode==='dark'?'white':'black'}}>
         <h2>Your text summery</h2>
         <p>{text.split(" ").length} words and {text.length} characters</p>
         <p>{0.008 * text.split(" ").length} Minutes to read</p>
         <h3>Preview</h3>
-        <p>{text}</p>
+        <p>{text.length>0?text:"Enter something into the text box to preview it here"}</p>
     </div>
     </>
   )
